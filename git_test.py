f4a1875090da12c6c4f5e329f8be3da588cd1e76
Uku Orgse <orgseuku@gmail.com>
2024-03-25 20:12:48 +0200

    lisatud Javier on äge

--- git_test.py
+++ git_test.py
@@ -3,7 +3,7 @@ sõne = input('Sisestage sõne: ')
 
 for i in range(arv):
     try:
-        print(sõne[i], end='-')
+        print(sõne[i], end=' - ')
+        print("Javier on äge!!!")
     except IndexError:
-        print("\narv on sõnest suurem")
         break
